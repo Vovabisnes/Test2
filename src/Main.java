7618bad2537ea26f38341a3c969a86d01203d122
Volodymyr <vovawestland@gmail.com>
2022-09-28 20:34:05 +0200

    Version 2.0

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Quinscape!");
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
